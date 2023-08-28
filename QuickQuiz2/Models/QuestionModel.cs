ffa34e88348de37e41e1c4c998b71a1cbaa58986
serein3 <jakubmichalski9818@gmail.com>
2023-08-28 11:59:47 +0200

    Add Initial DB Migration

---
+++ QuickQuiz2/Models/QuestionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickQuiz2.Models
+{
+    public class QuestionModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Category { get; set; } = null!;
+        public string Answer { get; set; } = null!;
+        public string ImagePath { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string HardDiffDesc { get; set; } = null!;
+    }
+}
