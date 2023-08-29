40382b66f8e59504f3af4f84d30fad54e89520a4
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2023-08-29 13:12:04 +0200

    Creating QuestionsController.cs

---
+++ QuickQuiz2/Controllers/QuestionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using QuickQuiz2.Models;
+using QuickQuiz2.Repos.QuestionsRepo;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuickQuiz2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IQuestionsRepo questionsRepo;
+
+        public QuestionsController(IQuestionsRepo questionsRepo)
+        {
+            this.questionsRepo = questionsRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<QuestionModel>?>> GetAll()
+        {
+            var questions = await this.questionsRepo.GetAllQuestions();
+
+            if (questions == null)
+            {
+                return NotFound("Could not find any questions");
+            }
+            else
+            {
+                return Ok(questions);
+            }
+        }
+
+
+
+
+
+    }
+}
