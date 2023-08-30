2100955608d78842a38be401281fe8310bfc8b45
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-08-30 09:51:48 +0200

    Inb4 switching to .NET guide
    Currently newing up a httpclient to use as fetching, however the .NET guide i found tells me i should use an instance of the controllers themselfes in the tests. Saving before switching it up.

---
+++ BackendTesterXUnit/QuestionsControllerTester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTesterXUnit
+{
+    public class QuestionsControllerTester
+    {
+        HttpClient httpClient;
+
+        public QuestionsControllerTester()
+        {
+            // Arrange httpClient
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44426/");
+        }
+
+        [Fact]
+        public async void GetAllQuestions_SuccessfullyFetched()
+        {
+            var response = await httpClient.GetAsync("/weatherforecast");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
