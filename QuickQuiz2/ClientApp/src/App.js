3ab0581c4a84a87c9794bd8ff03cd614035e5531
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2023-08-30 14:54:32 +0200

    Frontend update with Desmond

--- QuickQuiz2/ClientApp/src/App.js
+++ QuickQuiz2/ClientApp/src/App.js
@@ -3,12 +3,31 @@ import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
+import { apiHelper } from './services/ApiHelper';
+
 
 
 function App() {
+  const [question, setQuestion] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const question = await apiHelper.getQuestionById(2);
+        console.log(question);
+        setQuestion(question);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return ( 
     <div>
+      {question ? <img src={`/images/${question.imagePath}`} alt="" className="item-image" /> : 0}
       
+      <h2>Hej Jerry, är detta ok?</h2>
 
   </div> );
 }
