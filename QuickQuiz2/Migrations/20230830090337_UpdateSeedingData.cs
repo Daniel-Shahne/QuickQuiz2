f6f03cb9064d652feaa0cad3793c815da510b2b1
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2023-08-30 11:21:17 +0200

    SeedingData2
    Changed name id 39

---
+++ QuickQuiz2/Migrations/20230830090337_UpdateSeedingData.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickQuiz2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImagePath",
+                value: "alpaca.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImagePath",
+                value: "meerkat.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImagePath",
+                value: "panda.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImagePath",
+                value: "fennecfox.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImagePath",
+                value: "seaotter.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImagePath",
+                value: "seahorse.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImagePath",
+                value: "rattlesnake.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImagePath",
+                value: "Alpaca.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImagePath",
+                value: "Meerkat.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImagePath",
+                value: "Panda.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImagePath",
+                value: "FennecFox.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImagePath",
+                value: "SeaOtter.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImagePath",
+                value: "SeaHorse.png");
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImagePath",
+                value: "Rattlesnake.png");
+        }
+    }
+}
---
+++ QuickQuiz2/Migrations/20230830091942_UpdateSeedingdata2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickQuiz2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedingdata2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 39,
+                column: "ImagePath",
+                value: "hummingbird.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 39,
+                column: "ImagePath",
+                value: "albatross.png");
+        }
+    }
+}
