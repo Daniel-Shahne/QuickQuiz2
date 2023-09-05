644fdee6cb90f654daef80597b1841aa80534d82
Dennislindstrom <113332155+Dennislindstrom@users.noreply.github.com>
2023-09-05 14:44:07 +0200

    Added WonkyImage

---
+++ QuickQuiz2/ClientApp/src/components/WonkyShapes/WonkyImages/ShapeForWonkyImgs/WonkyImageSvg1.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+function WonkyImageSvg1({sizeMultiplier, rotateDegree}) {
+    let sizeMultiplierNum = parseFloat(sizeMultiplier);
+
+  const [calculatedWidth, setCalculatedWidth] = useState(
+    117 * sizeMultiplierNum
+  );
+  const [calculatedHeight, setCalculatedHeight] = useState(
+    133 * sizeMultiplierNum
+  );
+    return (<div>
+        <svg width={calculatedWidth} 
+        height={calculatedHeight} 
+        viewBox="0 0 117 133" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+    style={{ transform: `rotate(${rotateDegree}deg)`}}>
+<g filter="url(#filter0_d_67_203)">
+<path d="M111 3L6 37.7691V128L111 78.495V3Z" fill="#88E975"/>
+<path d="M111 3L6 37.7691V128L111 78.495V3Z" stroke="white" stroke-width="4"/>
+</g>
+<defs>
+<filter id="filter0_d_67_203" x="0" y="0.230957" width="117" height="138.923" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_67_203"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_67_203" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+    </div>  );
+}
+
+export default WonkyImageSvg1;
---
+++ QuickQuiz2/Migrations/20230828095748_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickQuiz2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HardDiffDesc = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
