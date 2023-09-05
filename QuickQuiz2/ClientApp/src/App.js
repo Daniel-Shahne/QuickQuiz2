7bff976ec90c446551612711c0abfb114bd5c8f9
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-09-05 11:40:56 +0200

    Resizing via hardcoding works now
    For some reason the website wont update when saving the hardcoded value, it has to actually be refreshed. However it works so long as the value isnt intended to be changed while running

--- QuickQuiz2/ClientApp/src/App.js
+++ QuickQuiz2/ClientApp/src/App.js
@@ -31,10 +31,10 @@ function App() {
   return ( 
     <div>
       <h2>Hej Jerry, är detta ok?</h2>
-      <WonkyShapeText1 shapeFill="#853AC8" sizeMultiplier="1.3"/>
+      <WonkyShapeText1 shapeFill="#853AC8" sizeMultiplier="1.2"/>
       <WonkyShapeText1 shapeFill="#00AC87" sizeMultiplier="1.5"/>
       <br/>
-      <WonkyContainerText shapeId="1" text="alhamdillulah" shapeFill="#00AC87" sizeMultiplier="1.0"/>
+      <WonkyContainerText shapeId="1" text="alhamdillulah" shapeFill="#00AC87" sizeMultiplier="1.3"/>
   </div> );
 }
 
