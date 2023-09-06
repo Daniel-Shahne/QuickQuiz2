042c69ca32fb90cb4fe9ca256efefe63d27fb999
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-09-06 10:30:45 +0200

    Update App.js
    reverted back to main

--- QuickQuiz2/ClientApp/src/App.js
+++ QuickQuiz2/ClientApp/src/App.js
@@ -5,15 +5,6 @@ import { Layout } from './components/Layout';
 import './custom.css';
 import { apiHelper } from './services/ApiHelper';
 
-// Wonky Shapes import
-import WonkyShapeText1 from './components/WonkyShapes/WonkyTexts/shapesForWonkyTexts/WonkyShapeText1';
-import WonkyShapeText2 from './components/WonkyShapes/WonkyTexts/shapesForWonkyTexts/WonkyShapeText2';
-
-// Wonky text container import
-import WonkyContainerText from './components/WonkyShapes/WonkyTexts/WonkyContainerText/WonkyContainerText';
-import WonkyImageSvg1 from './components/WonkyShapes/WonkyImages/shapesForWonkyImages/WonkyImageSvg1';
-import WonkyShapeChar1 from './components/WonkyShapes/WonkyTexts/shapesForWonkyTexts/WonkyShapeChar1';
-import WonkyShapeChar2 from './components/WonkyShapes/WonkyTexts/shapesForWonkyTexts/WonkyShapeChar2';
 
 
 function App() {
@@ -34,22 +25,9 @@ function App() {
 
   return ( 
     <div>
+      {question ? <img src={`/images/animals/${question.imagePath}`} alt="" className="item-image" /> : 0}
+      
       <h2>Hej Jerry, är detta ok?</h2>
-      <WonkyShapeText1 shapeFill="#853AC8" sizeMultiplier="1.1"/>
-      <WonkyShapeText2 shapeFill="#00AC87" sizeMultiplier="1.2"/>
-      <br/>
-      <WonkyContainerText shapeId="1" text="Play" shapeFill="#00AC87" sizeMultiplier="1.5" fontSize="15px"/>
-      <WonkyContainerText shapeId="2" text="Learn" shapeFill="#00AC87" sizeMultiplier="1.5" fontSize="15px"/>
-      <br/>
-      <WonkyImageSvg1 sizeMultiplier="1.0" rotateDegree="0"/>
-      <br/>
-      <WonkyShapeChar1 sizeMultiplier="1.0" shapeFill="#853AC8"/>
-      <WonkyShapeChar2 sizeMultiplier="1.0" shapeFill="#00AC87"/>
-      <br/>
-      <WonkyContainerText shapeId="3" text="A" shapeFill="#00AC87" sizeMultiplier="1.5" fontSize="15px"/>
-      <WonkyContainerText shapeId="4" text="L" shapeFill="#853AC8" sizeMultiplier="1.5" fontSize="15px"/>
-
-
 
   </div> );
 }
