17a52569dcf43525a8e4ec87b4e69daa43adbc14
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-09-07 11:01:42 +0200

    Answer index cycling working
    Just cycling the answer index, and what is shown depending on it, is working as expected. However no other game logic is currently implemented.

--- QuickQuiz2/ClientApp/src/pages/Game/GamePage.jsx
+++ QuickQuiz2/ClientApp/src/pages/Game/GamePage.jsx
@@ -11,6 +11,22 @@ function GamePage() {
   const [takenQuestionIndexes, setTakenQuestionIndexes] = useState([]);
   // Holds the index of the current question
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // Holds the index of the current answer
+  const [activeAnswerIndex, setActiveAnswerIndex] = useState(0);
+
+  /** Due to allQuestions being instantiated as null, the values of
+   * the index variables can only be set after the data has been fetched
+   */
+  useEffect(() => {
+    if (allQuestions !== null) {
+      setActiveQuestionIndex(() =>
+        Math.floor(Math.random() * allQuestions.length)
+      );
+      setActiveAnswerIndex(() =>
+        Math.floor(Math.random() * allQuestions.length)
+      );
+    }
+  }, allQuestions);
 
   // Holds how many answers have cycled since the last correct one
   const [cycledAnswersSinceCorrect, setCycledAnswersSinceCorrect] = useState(0);
@@ -43,22 +59,63 @@ function GamePage() {
     setActiveQuestionIndex(() => nextQuestionIndex);
   }
 
-  /** Cycles the question answer every time interval */
+  /** Triggers every time interval to set the index of the to be
+   * displayed answer either:
+   *
+   * The correct answer if cycles since last correct answer shown
+   * exceeds limit determined randomly
+   *
+   * OR
+   *
+   * The wrong answer if cycles dont exceed.
+   */
   useEffect(() => {
-    // Set up the interval
-    const timer = setInterval(() => {
-      // Increment cycles since last correct answer
-      setCycledAnswersSinceCorrect((prevState) => prevState + 1);
-    }, 2000); // Every 2 seconds
+    if (allQuestions !== null) {
+      const timer = setInterval(() => {
+        // Increment cycles since last correct answer
+        setCycledAnswersSinceCorrect((prevState) => prevState + 1);
 
-    // Clear the interval when the component unmounts
-    return () => clearInterval(timer);
-  }, []);
+        console.log(cycledAnswersSinceCorrect);
+
+        // Variable containing the next answer index (to be changed below)
+        let nextAnswerIndex = Math.floor(Math.random() * allQuestions.length);
+
+        // If current cycles exceed determined amount for correct
+        // answer then the correct answer index is chosen
+        if (cycledAnswersSinceCorrect >= randomCycleAmount) {
+          nextAnswerIndex = activeQuestionIndex;
+          setCycledAnswersSinceCorrect(() => 0);
+          setRandomCycleAmount(() => Math.floor(Math.random() * 8) + 1);
+        }
+        // Displays wrong question answer
+        else {
+          // Rerolls nextAnswerIndex if it happens to be the active index
+          while (nextAnswerIndex === activeQuestionIndex) {
+            nextAnswerIndex = Math.floor(Math.random() * allQuestions.length);
+          }
+        }
+
+        // Save the determined index of next answer to state variable
+        setActiveAnswerIndex(() => nextAnswerIndex);
+      }, 1000); // Time interval for index determination
+
+      // Clear the interval when the component unmounts
+      return () => clearInterval(timer);
+    }
+  }, [allQuestions, cycledAnswersSinceCorrect, randomCycleAmount]);
 
   return (
     <div>
-      <h1>Current question is: {allQuestions[activeQuestionIndex].answer}</h1>
-      <h1>Current cycled anser is: </h1>
+      {allQuestions ? (
+        <div>
+          <h1>
+            Current question is: {allQuestions[activeQuestionIndex].answer}
+          </h1>
+          <h1>
+            Current cycled anser is: {allQuestions[activeAnswerIndex].answer}
+          </h1>
+        </div>
+      ) : null}
     </div>
   );
 }
