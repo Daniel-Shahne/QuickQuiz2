8445ae2c13ca630588c070f24a5c5132bdbe2b7f
serein3 <jakubmichalski9818@gmail.com>
2023-09-07 10:12:35 +0200

    Updates
    Changed background css in order to keep it to the page only, removed debug messages.

--- QuickQuiz2/ClientApp/src/pages/SelectDifficulty/SelectDifficultyPage.jsx
+++ QuickQuiz2/ClientApp/src/pages/SelectDifficulty/SelectDifficultyPage.jsx
@@ -7,29 +7,30 @@ import { useContext } from "react";
 
 function SelectDifficultyPage() {
 
-  const {difficulty, setDifficulty} = useContext(AppContext);
+  const {setDifficulty} = useContext(AppContext);
 
   function setEasyDifficulty() {
     setDifficulty('easy')
-    console.log(difficulty)
   }
 
   function setHardDifficulty() {
     setDifficulty('hard')
-    console.log(difficulty)
   }
 
   return ( 
-    <div className={difficultyStyle.container}>
-      <h1>Choose Difficulty!</h1>
+    <div className={difficultyStyle.backgroundContainer}>
+      <div className={difficultyStyle.container}>
+        <h1>Choose Difficulty!</h1>
 
-      <Link to='/howtoplay' onClick={setEasyDifficulty}>
-        <WonkyContainerText shapeId='1' text='Easy' shapeFill='#00AC87' sizeMultiplier='4' fontSize='30px'/>
-      </Link>
-      <Link to='/howtoplay' onClick={setHardDifficulty}>
-        <WonkyContainerText shapeId='2' text='Hard' shapeFill='#853AC8' sizeMultiplier='4' fontSize='30px'/>
-      </Link>
-    </div> 
+        <Link to='/howtoplay' onClick={setEasyDifficulty}>
+          <WonkyContainerText shapeId='1' text='Easy' shapeFill='#00AC87' sizeMultiplier='4' fontSize='30px'/>
+        </Link>
+        <Link to='/howtoplay' onClick={setHardDifficulty}>
+          <WonkyContainerText shapeId='2' text='Hard' shapeFill='#853AC8' sizeMultiplier='4' fontSize='30px'/>
+        </Link>
+      </div> 
+    </div>
+    
     
     );
 }
