22fd67eae0d5ad59891c23413674dde0af7b4465
Annica Westford <annica.westford@gmail.com>
2023-09-08 14:40:40 +0200

    Begin results page

--- QuickQuiz2/ClientApp/src/pages/Results/ResultsPage.jsx
+++ QuickQuiz2/ClientApp/src/pages/Results/ResultsPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import resultsPageStyle from "./resultspage.module.css";
-import resultsPageBG from "QuickQuiz2/ClientApp/src/pages/Home/landingpageBG.png";
 
 function ResultsPage() {
-  return <div className={resultsPageStyle.resultsContainer}></div>;
+  return (
+    <div className={resultsPageStyle.resultsBGContainer}>
+      <div className={resultsPageStyle.resultsGridContainer}>
+        <div>Player 1 result</div>
+        <div>Play again button</div>
+        <div>Return to menu button</div>
+        <div>Player 2 result</div>
+      </div>
+    </div>
+  );
 }
 
 export default ResultsPage;
