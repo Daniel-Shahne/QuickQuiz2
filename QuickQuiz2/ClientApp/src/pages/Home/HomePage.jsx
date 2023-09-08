71430083739f93696bba33beeb906e8e21ec8c48
Annica Westford <annica.westford@gmail.com>
2023-09-08 10:17:32 +0200

    Begin landing page layout

--- QuickQuiz2/ClientApp/src/pages/Home/HomePage.jsx
+++ QuickQuiz2/ClientApp/src/pages/Home/HomePage.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
+import homePageStyle from "./homepage.module.css";
+import wonkyHeadingShape from "./wonky-heading-shape.png";
 
 function HomePage() {
   const { difficulty } = useContext(AppContext);
 
-  return <h1>{difficulty}</h1>;
+  return (
+    <div className={homePageStyle.gridContainer}>
+      <div>Not</div>
+      <div className={homePageStyle.heading}>
+        <img src={wonkyHeadingShape} alt="" />
+        <div className={homePageStyle.textOverlay}>
+          <h1>Quick Quiz</h1>
+          <h3>Think fast, Act faster!</h3>
+        </div>
+      </div>
+      <div>Björn</div>
+      <div
+        className={`${homePageStyle.gridColSpan2} ${homePageStyle.gridRowSpan2}`}
+      >
+        Quokka
+      </div>
+      <div className={homePageStyle.playBtn}>Play</div>
+      <div className={homePageStyle.learnBtn}>Learn</div>
+      <div className={homePageStyle.vovve}>Vovve</div>
+    </div>
+  );
 }
 
 export default HomePage;
