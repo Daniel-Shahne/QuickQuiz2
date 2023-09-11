eb3fc1b21a4d3e801d0cf27c6ba2722ee4691cce
Cisssla <113521823+Cisssla@users.noreply.github.com>
2023-09-11 11:01:28 +0200

    import components

--- QuickQuiz2/ClientApp/src/pages/Results/ResultsPage.jsx
+++ QuickQuiz2/ClientApp/src/pages/Results/ResultsPage.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import resultsPageStyle from "./resultspage.module.css";
+import resultwonky1 from "./resultwonky1.png";
+import resultwonky2 from "./resultwonky2.png";
+import WonkyContainerText from "../../components/WonkyShapes/WonkyTexts/WonkyContainerText/WonkyContainerText";
+
+
 
 function ResultsPage() {
   return (
     <div className={resultsPageStyle.resultsBGContainer}>
-      <div className={resultsPageStyle.resultsGridContainer}>
-        <div>Player 1 result</div>
-        <div>Play again button</div>
-        <div>Return to menu button</div>
-        <div>Player 2 result</div>
+      <div className={resultsPageStyle.resultsFlexContainer}></div>
+      <div className={resultsPageStyle.flexrow1}>
+        <img src={resultwonky1} />
+        <img src={resultwonky2} />
+      </div>
+      <div>
+        <WonkyContainerText
+          onClickUrl="/selectdifficulty"
+          shapeId="2"
+          text="Play again"
+          shapeFill="#853AC8"
+          sizeMultiplier="4"
+          fontSize="30px"
+        />
       </div>
     </div>
   );
