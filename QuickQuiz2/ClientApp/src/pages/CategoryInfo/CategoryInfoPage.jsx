79fa6639e6f68f43b67fbba3b882e6d6b416a7f9
JosefinRasand <113332145+JosefinRasand@users.noreply.github.com>
2023-09-13 14:09:13 +0200

    Started logic to cycle through catergory items

--- QuickQuiz2/ClientApp/src/pages/CategoryInfo/CategoryInfoPage.jsx
+++ QuickQuiz2/ClientApp/src/pages/CategoryInfo/CategoryInfoPage.jsx
@@ -4,17 +4,24 @@ import BackArrow from "../../components/BackArrow/BackArrow";
 import CategoryItemCircle from "../../components/circles/CategoryItemCircle";
 import { AppContext } from "../../context/AppContext";
 import { useParams } from "react-router-dom";
-
+import arrowLeft from "./arrowLeft.png"
+import arrowRight from "./arrowRight.png"
 function CategoryInfoPage() {
-  const { index } = useParams();
+  let { index } = useParams();
 
   const { allQuestions } = useContext(AppContext);
+
+  function nextIndex(){
+    index++;
+    console.log(index);
+  }
 
   return (
     <div className={CategoryInfoStyle.bgContainer}>
       <div className={CategoryInfoStyle.backarrow}>
         <BackArrow onClickUrl="/home" />
       </div>
+      <div><img src={arrowLeft}/></div>
       <div className={CategoryInfoStyle.playItemContainer}>
         {allQuestions ? (
             <div className={CategoryInfoStyle.imgContainer}>
@@ -28,8 +35,10 @@ function CategoryInfoPage() {
           <p>{allQuestions[index].description}</p>
           </div>
           </div>
+          
         ) : null}
       </div>
+      <div><img src={arrowRight} onClick={nextIndex}/></div>
     </div>
   );
 }
